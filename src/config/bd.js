3edb4cb41608f1911c101ed3ad23a6d45a70738c
Lucas Coimbra Rocha Lago <92893420+LucasCoimbrax@users.noreply.github.com>
2023-07-03 16:47:25 -0300

    teste

--- src/config/bd.js
+++ src/config/bd.js
@@ -4,7 +4,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('projetoweb', 'ProjetoWeb Vacinas', 'Pr0j3t0W3bV4c1n45',
 {
-    dialect: 'mssql', host:'localhost', port: 63333
+    dialect: 'mssql', host:'localhost', port: 63333 | 50658
 });
  
 database.sync();
