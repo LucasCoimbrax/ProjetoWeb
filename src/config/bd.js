4715e3db31656639ec61386b50b91a1f86871568
maichakWesley <72577791+maichakWesley@users.noreply.github.com>
2023-07-06 13:04:30 -0300

    tela login médico

--- src/config/bd.js
+++ src/config/bd.js
@@ -4,7 +4,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('projetoweb', 'ProjetoWeb Vacinas', 'Pr0j3t0W3bV4c1n45',
 {
-    dialect: 'mssql', host:'localhost', port: 50658
+    dialect: 'mssql', host:'localhost', port: 65054
 });
  
 database.sync();
