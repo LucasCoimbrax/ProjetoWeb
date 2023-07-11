a2f267336f70051f4413880e5e4231bf14f2fcd6
Lucas Coimbra Rocha Lago <92893420+LucasCoimbrax@users.noreply.github.com>
2023-07-11 13:32:39 -0300

    Lista de vacinas do usuário

--- src/controllers/cadasterVacine.js
+++ src/controllers/cadasterVacine.js
@@ -2,33 +2,38 @@ const vacine = require("../model/vacine");
 const user = require("../model/user");
 
 module.exports = {
-    async vacine(req, res){
-        res.render('../views/cadasterVacine',{edvGet: req.query.edv})
-    },
+  async vacine(req, res) {
+    res.render("../views/cadasterVacine", { edvGet: req.query.edv });
+  },
 
   async cadasterVacine(req, res) {
     const dados = req.body;
     const edv = req.query.edv;
 
-    try{
+    try {
+      if (
+        !edv ||
+        !dados.name ||
+        !dados.fabricante ||
+        !dados.infos ||
+        !dados.doses ||
+        !dados.periodoAtivo
+      ) {
+        throw new Error("401 - Bad Request, Forms Invalid!");
+      }
 
-        if(!dados.name || !dados.fabricante || !dados.infos || !dados.doses || !dados.periodoAtivo){
-            throw new Error("401 - Bad Request, Forms Invalid!")
-        }
+      const vacinaID = await vacine.create({
+        Nome: dados.name,
+        Fabricante: dados.fabricante,
+        Infos: dados.infos,
+        Doses: dados.doses,
+        PeriodoAtivo: dados.periodoAtivo,
+        EDV: edv,
+      });
 
-        const vacinaID = await vacine.create({
-            Nome: dados.name,
-            Fabricante: dados.fabricante,
-            Infos: dados.infos,
-            Doses: dados.doses,
-            PeriodoAtivo: dados.periodoAtivo,
-            EDV: edv
-        });
-
-        res.redirect(`/perfilAdmin?edv=${req.query.edv}`)
-    }catch(e){
-        res.redirect("/cadasterVacine");
+      res.redirect(`/perfilAdmin?edv=${req.query.edv}`);
+    } catch (e) {
+      res.redirect("/cadasterVacine");
     }
-  }
-  
+  },
 };
--- src/controllers/perfilUser.js
+++ src/controllers/perfilUser.js
@@ -1,23 +1,54 @@
-const user = require('../model/user');
+const user = require("../model/user");
+const vacine = require("../model/vacine");
 
 module.exports = {
-    async user(req, res){
-        const u = await user.findByPk(req.query.EDV)
+  async user(req, res) {
+    const u = await user.findByPk(req.query.EDV);
 
-        if(!user){
-            return
-        }
+    let edvGet = req.query.EDV;
 
-        res.render('../views/perfilUser.ejs', {user:u})
-    },
-
-    async insertFoto(req, res){
-        console.log(req.body)
-        const u = await user.findByPk(req.query.EDV)
-        await user.update({
-            Foto: req.file.filename
-        },
-        {where: {EDV: req.query.EDV}})
-        res.redirect(`/perfilUser?EDV=${req.query.EDV}`)
+    try{
+        const vaccines = await vacine.findAll({
+            raw: true,
+            attributes: [
+              "Nome",
+              "Fabricante",
+              "Infos",
+              "Doses",
+              "PeriodoAtivo",
+              "createdAt",
+            ],
+            where: { EDV: edvGet },
+          });
+          let vacines = "";
+          for (let i = 0; i < vaccines.length; i++) {
+            const createdAt = new Date(vaccines[i].createdAt);
+            const formattedDate = createdAt.toLocaleDateString("pt-BR");
+            vacines += "<tr>";
+            vacines += "<td>" + vaccines[i].Nome + "</td>";
+            vacines += "<td>" + vaccines[i].Fabricante + "</td>";
+            vacines += "<td>" + vaccines[i].Infos + "</td>";
+            vacines += "<td>" + vaccines[i].Doses + "</td>";
+            vacines += "<td>" + vaccines[i].PeriodoAtivo + "</td>";
+            vacines += "<td>" + formattedDate + "</td>";
+            vacines += "</tr>";
+          }
+      
+          res.render("../views/perfilUser.ejs", { user: u, vacines });
+    }catch (e) {
+        re.redirect("/")
     }
-}
+  },
+
+  async insertFoto(req, res) {
+    console.log(req.body);
+    const u = await user.findByPk(req.query.EDV);
+    await user.update(
+      {
+        Foto: req.file.filename,
+      },
+      { where: { EDV: req.query.EDV } }
+    );
+    res.redirect(`/perfilUser?EDV=${req.query.EDV}`);
+  }
+};
